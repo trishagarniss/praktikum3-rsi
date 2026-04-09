c6f8a2fa88e492727884afd80d7e7fb130e8c1bc
trishagarniss <trishagarniss@gmail.com>
2026-04-09 14:07:57 +0700

    done

--- fullstack-app/backend/src/backend/app.py
+++ fullstack-app/backend/src/backend/app.py
@@ -11,3 +11,9 @@ def read_root():
     return {"message": "Server Backend Sedang Berjalan Cuyy!"}
 
 app.include_router(role_route.router)
+
+# Tambahkan ini di paling bawah
+if __name__ == "__main__":
+    import uvicorn
+    # Pakai "app:app" karena nama file kamu app.py
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
