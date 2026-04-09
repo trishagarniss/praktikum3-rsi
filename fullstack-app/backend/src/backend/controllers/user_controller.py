e5ca6c9182801a17e559fbada79cedcf380e6ad1
deskafimcode <deskafim.code@gmail.com>
2026-04-09 21:39:43 +0700

    Final Insyaallah

--- fullstack-app/backend/src/backend/controllers/user_controller.py
+++ fullstack-app/backend/src/backend/controllers/user_controller.py
@@ -1,22 +1,23 @@
 from sqlmodel import Session
 from src.backend.services import user_service
-from src.backend.dto.user_dto import User,UserInput,UserUpdate
+from src.backend.dto.user_dto import UserInput,UserUpdate
 
-def create_user_controller(user : UserInput):
-    return user_service.tambah_user(data_user = user)
+
+def create_user_controller(user : UserInput,db:Session):
+    return user_service.tambah_user(data_user = user,db=db)
 
 # 2. READ ALL
-def get_all_user_controller():
-    return user_service.tampilkan_user()
+def get_all_user_controller(db:Session):
+    return user_service.tampilkan_user(db=db)
 
 # 3. READ BY ID
-def get_user_by_id_controller(user_id: int):
-    return user_service.tampilkan_user(id = user_id)
+def get_user_by_id_controller(user_id: int,db:Session):
+    return user_service.tampilkan_user(id = user_id,db=db)
 
 # 4. UPDATE
-def update_user_controller(data : UserUpdate):
-    return user_service.edit_user(data_user= data)
+def update_user_controller(data : UserUpdate,db:Session):
+    return user_service.edit_user(data_user= data,db=db)
 
 # 5. DELETE
-def delete_user_controller(id: int):
-    return user_service.hapus_user(id_input=id)
+def delete_user_controller(id: int,db:Session):
+    return user_service.hapus_user(id_input=id,db=db)
--- fullstack-app/backend/src/backend/dto/user_dto.py
+++ fullstack-app/backend/src/backend/dto/user_dto.py
@@ -1,15 +1,16 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     first_name: str
     last_name: str
     whatsapp: str
-    created_at: str
-    updated_at: str
-
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+    
 class UserInput(BaseModel):
     first_name: str
     last_name: str
