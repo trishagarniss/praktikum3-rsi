f99bff02de456ffcbec516e8a05634e735c76142
trishagarniss <trishagarniss@gmail.com>
2026-05-14 20:07:23 +0700

    .

--- fullstack-app/backend/src/backend/utils/security.py
+++ fullstack-app/backend/src/backend/utils/security.py
@@ -6,11 +6,8 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 def get_password_hash(password: str) -> str:
     pwd_bytes = password.encode('utf-8')
-
     salt = bcrypt.gensalt()
-    
     hashed_password = bcrypt.hashpw(pwd_bytes, salt)
-    
     return hashed_password.decode('utf-8')
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
